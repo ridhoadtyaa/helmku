f54d0ddc117bae6d567929d4b83348d7681f21bd
Izzeldin Addarda <zeldin26@gmail.com>
2021-11-14 18:49:32 +0700

    fixed title heading

--- app/Views/dashboard/kategori/create.php
+++ app/Views/dashboard/kategori/create.php
@@ -7,7 +7,7 @@
 <?= $this->section('content') ?>
 <section class="section">
     <div class="section-header">
-    <h1>?= $title ?></h1>
+    <h1><?= $title ?></h1>
     <div class="section-header-breadcrumb">
         <div class="breadcrumb-item active"><a href="/dashboard">Dashboard</a></div>
         <div class="breadcrumb-item"><a href="/dashboard/produk"><?= $title ?></a></div>
