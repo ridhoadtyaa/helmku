e0c6929e8c5a02966704fe8f37f57656a10d57aa
Izzeldin Addarda <zeldin26@gmail.com>
2021-11-14 13:45:15 +0700

    add database credential

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => '',
+        'database' => 'helmku',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
