ff46b49cf5d5db1ab2eb4452643e12d392de63ac
Izzeldin Addarda <zeldin26@gmail.com>
2021-11-14 12:48:06 +0700

    updated timezone & database credential

--- app/Config/App.php
+++ app/Config/App.php
@@ -108,7 +108,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $appTimezone = 'America/Chicago';
+    public $appTimezone = 'Asia/Jakarta';
 
     /**
      * --------------------------------------------------------------------------
--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,7 +33,7 @@ class Database extends Config
     public $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
+        'username' => 'root',
         'password' => '',
         'database' => '',
         'DBDriver' => 'MySQLi',
