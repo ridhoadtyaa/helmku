6b7411afee9188662790a05cf092beaf7a8bb2a2
ridhoadtyaa <r2603an@gmail.com>
2021-11-18 15:56:35 +0700

    empty cart

--- app/Views/produk.php
+++ app/Views/produk.php
@@ -116,27 +116,19 @@
 
 <?= $this->section('javascript') ?>
 <script>
-// Set the date we're counting down to
 const countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+const x = setInterval(() => {
 
-// Update the count down every 1 second
-const x = setInterval(function() {
-
-  // Get today's date and time
   const now = new Date().getTime();
     
-  // Find the distance between now and the count down date
   const distance = countDownDate - now;
     
-  // Time calculations for days, hours, minutes and seconds
   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-  // Output the result in an element with id="demo"
   document.getElementById("flashTime").innerHTML = `${hours} : ${minutes} : ${seconds}`;
     
-  // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("flashTime").innerHTML = "EXPIRED";
